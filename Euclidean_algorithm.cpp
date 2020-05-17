d899a5d406b2789a181e6001421343e96e9938f5
Ozzie07 <58475003+Ozzie07@users.noreply.github.com>
2020-05-17 23:39:06 +0900

    Update Euclidean_algorithm.cpp
    primenumberfunc added

--- Euclidean_algorithm.cpp
+++ Euclidean_algorithm.cpp
@@ -4,3 +4,12 @@ int GCD(int &a, int &b){
 }
 
 int LCM(int a, int b) {return a * b / GCD(a, b);}
+
+//find Isit primenumber?
+bool prime(int n){
+  if(n<2) return false;
+  for (int i = 2; i*i<=n; i++)
+    if(n%i==0)
+      return false;
+  return true;
+}
